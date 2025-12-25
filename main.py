f006f14d3e8e017d0d7102994e7b539813af872a
sofyaberezhnaya <Berezhnayaa.sofya@yandex.ru>
2025-12-25 19:13:19 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import telebot, random
+from bot_logic import gen_pass
+from telebot.types import ReactionTypeEmoji
+from telebot import types
+
+bot = telebot.TeleBot("8433083621:AAGjpwTHNlZALkD_E2bE_-mRbmezrb-C5sE")
+  
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+  
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['generate_pass'])
+def gen_password(message):
+    password = gen_pass(10)
+    bot.reply_to(message, password)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['generate_emoj'])
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+@bot.chat_member_handler()
+def chat_m(message: types.ChatMemberUpdated):
+    old = message.old_chat_member
+    new = message.new_chat_member
+    if new.status == "member":
+        bot.send_message(message.chat.id,"Hello {name}!".format(name=new.user.first_name)) 
+   
+@bot.message_handler(func=lambda message: True)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+@bot.message_handler(commands=['flips_coins'])
+def flip_coin(message):
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
+
+bot.polling()
