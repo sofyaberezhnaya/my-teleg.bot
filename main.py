776f61bedec1af0df86bfafebd91991a34251076
sofyaberezhnaya <Berezhnayaa.sofya@yandex.ru>
2026-01-07 12:00:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from telebot import types
 
 bot = telebot.TeleBot("8433083621:AAGjpwTHNlZALkD_E2bE_-mRbmezrb-C5sE")
   
+# Обработчик команды '/start' и '/hello'
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
-  
+    
+# Обработчик команды '/heh'
 @bot.message_handler(commands=['heh'])
 def send_heh(message):
     count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
@@ -22,6 +24,7 @@ def send_bye(message):
 def gen_password(message):
     password = gen_pass(10)
     bot.reply_to(message, password)
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
@@ -44,6 +47,7 @@ def send_reaction(message):
     emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
     bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
 
+
 @bot.message_handler(commands=['flips_coins'])
 def flip_coin(message):
     flip = random.randint(0, 2)
@@ -51,5 +55,29 @@ def flip_coin(message):
         return "ОРЕЛ"
     else:
         return "РЕШКА"
+    
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling(skip_pending=True)# Skip pending skips old updates
+
+@bot.chat_join_request_handler()
+def make_some(message: telebot.types.ChatJoinRequest):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+bot.infinity_polling(allowed_updates=telebot.util.update_types)
+
+@bot.chat_join_request_handler()
+def make_some(message: telebot.types.ChatJoinRequest):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+bot.infinity_polling(allowed_updates=telebot.util.update_types)
 
 bot.polling()
